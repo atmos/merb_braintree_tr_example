3e8308449679cd131dcf84b8bd2e58afe612f51c
Corey Donohoe <atmos@atmos.org>
2008-12-29 16:41:06 -0700

    updating to merb 1.0.7

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,5 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.6.1"
+merb_gems_version = "1.0.7"
 dm_gems_version   = "0.9.8"
 
 #dependency 'rfuzz', '=0.9', :require_as => 'rfuzz/client'
@@ -8,7 +8,8 @@ dm_gems_version   = "0.9.8"
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version  
-dependency "merb-cache", merb_gems_version   
+dependency "merb-haml", merb_gems_version   
+dependency "merb-gen", merb_gems_version, :require_as => nil
 dependency "merb-helpers", merb_gems_version 
 dependency "merb-mailer", merb_gems_version  
 dependency "merb-slices", merb_gems_version  
@@ -17,6 +18,7 @@ dependency "merb-auth-more", merb_gems_version
 dependency "merb-auth-slice-password", merb_gems_version
 dependency "merb-param-protection", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
+dependency "merb_datamapper", merb_gems_version
  
 dependency "dm-core", dm_gems_version         
 dependency "dm-aggregates", dm_gems_version   
